a879333d6bea77e2d04c36b26ab331dcff06583d
teva-redpill <teva@redpill.paris>
2023-11-02 20:53:24 +0100

    build: improve card

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ function App() {
     <Router>
         <Routes>
           <Route exact path="/" element={<CardQR name='default'/>} />
-          <Route exact path="/parietal" element={<CardQR name='parietal' />} />
-          <Route exact path="/iceberg" element={<CardQR name='iceberg' />} />
-          <Route exact path="/golden" element={<CardQR name='golden' />} />
-          <Route exact path="/silver" element={<CardQR name ='silver' />} />
-          <Route exact path="/green" element={<CardQR name='green' />} />
-          <Route exact path="/blue" element={<CardQR name='blue' />} />
+          <Route exact path="/parietal" element={<CardQR name='parietal' dimensions='215x190' materials='acrylic on linen' price='5000 €' />} />
+          <Route exact path="/iceberg" element={<CardQR name='iceberg' dimensions='187x147' materials='acrylic on canvas' price='5000 €' />} />
+          <Route exact path="/golden" element={<CardQR name='golden-fleece' dimensions='116x104' materials='acrylic , spray on linen' price='5000 €'/>} />
+          <Route exact path="/silver" element={<CardQR name ='silver' dimensions='150x150' materials='acrylic, spray on canvas' price='1000 €'/>} />
+          <Route exact path="/green" element={<CardQR name='green' dimensions='150x150' materials='acrylic, spray on canvas' price='1000 €'/>} />
+          <Route exact path="/blue" element={<CardQR name='blue' dimensions='150x150' materials='acrylic, spray on canvas' price='1000 €'/>} />
+          <Route exact path="/atoms" element={<CardQR name='atoms' dimensions='200x143' materials='acrylic on canvas' price='5000 €'/>} />
+          <Route exact path="/energy" element={<CardQR name='energy' dimensions='187x144' materials='acrylic, spray on canvas' price='5000 €'/>} />
+          <Route exact path="/deepness" element={<CardQR name='deepness' dimensions='150x150' materials='acrylic on canvas' price='5000 €'/>} />
         </Routes>
       </Router>
     </div>
--- src/components/cardQR/index.js
+++ src/components/cardQR/index.js
@@ -2,25 +2,36 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React from 'react';
+
 // eslint-disable-next-line no-restricted-imports
 const CardQR = props => {
   const {
-    name
+    name,
+    dimensions,
+    materials,
+    price,
   } = props;
 
   const img_ = `/assets/img/${name}.jpg`
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img_} />
+    <div style={{fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px'}}>
+    <Card style={{  }}>
+      <Card.Title style={{marginLeft:'60px', marginBottom:'5px', fontSize:'28px'}}>Highart</Card.Title>
+      <Card.Img variant="top" style={{ width:'200px', backgroundColor:'#000000', padding:'10px'}} src={img_} />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title style={{marginLeft:'5%', fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)' }}>{name}</Card.Title>
+        <Card.Text style={{marginLeft:'5px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+        {materials}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Text style={{marginLeft:'3%', marginTop:'-15px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+        ({dimensions})
+        </Card.Text>
+        <Card.Text style={{marginLeft:'4%', marginTop:'-5px', fontSize:'25px' }}>
+         {price}
+        </Card.Text>
       </Card.Body>
     </Card>
+    </div>
   );
 }
 
