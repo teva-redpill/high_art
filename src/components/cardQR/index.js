dcff1f526582cb85ad700e2b5716e849663a1d8d
teva-redpill <teva@redpill.paris>
2023-11-03 19:03:38 +0100

    build: fix card

--- src/components/cardQR/index.js
+++ src/components/cardQR/index.js
@@ -13,14 +13,15 @@ const CardQR = props => {
   } = props;
 
   const img_ = `/assets/img/${name}.jpg`
+  
   return (
     <div style={{fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px'}}>
-    {name === 'me ' ? 
+    {name === 'me' ? 
     <Card>
-    <Card.Img variant="top" style={{ width:'300px', backgroundColor:'#000000', padding:'10px'}} src='/assets/img/me.jpeg' />
+    <Card.Img variant="top" style={{ width:'300px', padding:'10px'}} src='/assets/img/me.jpeg' />
     <Card.Body>
       <Card.Title style={{fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)' }}>HIGH ART</Card.Title>
-      <Card.Text style={{marginLeft:'5px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+      <Card.Text style={{marginLeft:'5px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)', width:'300px' }}>
       Independent artist whose creative projects revolves around abstracts, presented in a contemporary manner.
       Artist uses acrylic and a variety  of textures. The artist create large format art on canvas, conveying all the emotions and sensuality of the artist.
       Currently the artist lives in Paris.
@@ -29,17 +30,17 @@ const CardQR = props => {
   </Card>
    : 
     <Card>
-      <Card.Title style={{marginLeft:'60px', marginBottom:'5px', fontSize:'28px'}}>Highart</Card.Title>
-      <Card.Img variant="top" style={{ width:'200px', backgroundColor:'#000000', padding:'10px'}} src={img_} />
-      <Card.Body>
+      <Card.Title style={{marginLeft:'25px', marginBottom:'5px', fontSize:'28px'}}>Highart</Card.Title>
+      <Card.Img variant="top" style={{ width:'150px'}} src={img_} />
+      <Card.Body style={{justifyContent:'center', width:'150px'}}>
         <Card.Title style={{fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)' }}>{name}</Card.Title>
-        <Card.Text style={{marginLeft:'5px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+        <Card.Text style={{fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
         {materials}
         </Card.Text>
-        <Card.Text style={{marginLeft:'3%', marginTop:'-15px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+        <Card.Text style={{marginTop:'-15px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
         ({dimensions})
         </Card.Text>
-        <Card.Text style={{marginLeft:'4%', marginTop:'-5px', fontSize:'25px' }}>
+        <Card.Text style={{marginTop:'-5px', fontSize:'25px' }}>
          {price}
         </Card.Text>
       </Card.Body>
