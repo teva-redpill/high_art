d84b9c8d518e3df95b95f858fb1004ae376afd0e
teva-redpill <teva@redpill.paris>
2023-11-04 11:11:00 +0100

    fix: about

--- src/components/cardQR/index.js
+++ src/components/cardQR/index.js
@@ -15,32 +15,34 @@ const CardQR = props => {
   const img_ = `/assets/img/${name}.jpg`
   
   return (
-    <div style={{fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px'}}>
+    <div style={{display:'flex !important',justifyContent:'center', fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px'}}>
     {name === 'me' ? 
     <Card>
-    <Card.Img variant="top" style={{ width:'300px', padding:'10px'}} src='/assets/img/me.jpeg' />
+    <Card.Title style={{marginLeft:'110px', fontSize:'50px', fontWeight:'bold', color:'#000000' }}><b>HIGH ART</b></Card.Title>
+    <Card.Img variant="top" style={{maxWidth:'400px',padding:'20px'}} src='/assets/img/me.jpeg' />
     <Card.Body>
-      <Card.Title style={{fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)' }}>HIGH ART</Card.Title>
-      <Card.Text style={{marginLeft:'5px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)', width:'300px' }}>
-      Independent artist whose creative projects revolves around abstracts, presented in a contemporary manner.
-      Artist uses acrylic and a variety  of textures. The artist create large format art on canvas, conveying all the emotions and sensuality of the artist.
-      Currently the artist lives in Paris.
+      <Card.Text style={{justifyContent:'center !important', marginLeft:'20px', fontSize:'22px', color:'rgba(25, 25, 51, 0.7)', width:'400px', textAlign:'justify' }}>
+        <p style={{textAlign:'justify'}}>artist whose creative projects revolves around abstracts, presented in a contemporary manner.<br/>
+Minimalism, volume, textures, big format of canvas and linens.<br/>
+Art created by the emotions and energy.
+</p>
+      <p style={{textAlign:'justify'}}>
+Artist worked like  a fashion model in Europe and Asia ,  study at the fashion school - Marangoni in Milan. Always has been interested about different variety or creations.
+<br/>Nowadays artist live in Paris.</p>
       </Card.Text>
     </Card.Body>
   </Card>
    : 
     <Card>
-      <Card.Title style={{marginLeft:'25px', marginBottom:'5px', fontSize:'28px'}}>Highart</Card.Title>
+      <Card.Title style={{marginLeft:'23px',marginBottom:'10px', fontSize:'24px', fontWeight:'bold', color:'#000000' }}><b>HIGH ART</b></Card.Title>
       <Card.Img variant="top" style={{ width:'150px'}} src={img_} />
-      <Card.Body style={{justifyContent:'center', width:'150px'}}>
-        <Card.Title style={{fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)' }}>{name}</Card.Title>
-        <Card.Text style={{fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
+      <Card.Body style={{display:'flex !important'}}>
+        <Card.Title style={{justifyContent:'center', fontSize:'20px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'justify'}}>{name}</Card.Title>
+        <Card.Text style={{justifyContent:'center', marginLeft:'0px', fontSize:'20px', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'justify' }}>
         {materials}
-        </Card.Text>
-        <Card.Text style={{marginTop:'-15px', fontSize:'19px', color:'rgba(25, 25, 51, 0.7)' }}>
         ({dimensions})
         </Card.Text>
-        <Card.Text style={{marginTop:'-5px', fontSize:'25px' }}>
+        <Card.Text style={{marginLeft:'30px', marginTop:'-5px', fontSize:'25px' }}>
          {price}
         </Card.Text>
       </Card.Body>
