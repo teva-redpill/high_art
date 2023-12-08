0749efd313022d7a8919f6121d9675e06dd10535
teva-redpill <teva@redpill.paris>
2023-12-08 18:42:44 +0100

    build: add serials

--- src/components/serials/index.js
+++ src/components/serials/index.js
@@ -9,41 +9,42 @@ const Serials = props => {
 
   if (id === '1') 
   return (
-  <div>
+
+    <div style={{display:'flex !important',justifyContent:'center', fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px', backgroundColor:'#EFEFEF', align:'center', width:'240px', padding:'15px'}}>
     <Card>
       <Card.Body style={{display:'flex !important'}}>
-        <Card.Title style={{justifyContent:'center', fontSize:'30px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'justify'}}>
+        <Card.Title style={{justifyContent:'center', fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'justify'}}>
           "Energy"  2017
           </Card.Title>
-          <Card.Title style={{justifyContent:'center', fontSize:'30px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'justify'}}>
+          <Card.Title style={{justifyContent:'center', fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'justify'}}>
           "Black Rock" 2023
           </Card.Title>
-        <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'20px', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'left' }}>
+        <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'20px', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'left' }}>
         Acrylic, spray, acrylic pasta on canvas
         (187/144)
         </Card.Text>
-        <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'9px', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'center' }}>
-        @highart design
+        <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'9px', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'center' }}>
+        @highart design <br/>
         serial number : 08122022-1
         </Card.Text>
       </Card.Body>
     </Card>
     </div>
   )
-  if (id == '2')
+  if (id === '2')
   return (
-    <div>
+    <div style={{display:'flex !important',justifyContent:'center', fontFamily: "'Poppins', sans-serif", fontSize:'22px', marginTop:'20px', marginLeft:'40px', backgroundColor:'#EFEFEF', align:'center', width:'240px', padding:'15px'}}>
       <Card>
         <Card.Body style={{display:'flex !important'}}>
-          <Card.Title style={{justifyContent:'center', fontSize:'30px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'justify'}}>
+          <Card.Title style={{justifyContent:'center', fontSize:'22px', fontWeight:'bold', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'justify'}}>
             "Parietal"  2020
             </Card.Title>
-          <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'20px', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'left' }}>
+          <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'20px', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'left' }}>
           Acrylic on linen
           (215/190)
           </Card.Text>
-          <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'9px', color:'rgba(25, 25, 51, 0.7)', width:'150px', textAlign:'center' }}>
-          @highart design
+          <Card.Text style={{justifyContent:'left', marginLeft:'0px', fontSize:'9px', color:'rgba(25, 25, 51, 0.7)', width:'200px', textAlign:'center' }}>
+          @highart design <br/>
           serial number : 08122022-2
           </Card.Text>
         </Card.Body>
